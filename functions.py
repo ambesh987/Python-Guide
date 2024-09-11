8ff25d373b8ada89eda8d98ff3fc2125c38086d8
Ambesh <ambeshpm@gmail.com>
2024-09-11 19:20:35 +0530

    added functions

---
+++ functions.py
@@ -0,0 +1,21 @@
+# functions are reusable blocks of code that perform a specific task 
+
+# I have to write a program to add 2 numbers
+
+# a= int(input())
+# b= int(input())
+
+# ans= a+b
+# print(ans)
+
+def solve(a,b):
+    ans = a + b
+    return ans
+
+# print(solve(10,20))
+
+def multiply(x,y):
+    mult = x * y
+    return mult
+
+print(multiply(12,5))
